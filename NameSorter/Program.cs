00df2cb5c6568381a8ba4113a9d7e3c778444cd0
Chamila Pathirana <chamilagreat@gmail.com>
2025-09-29 10:27:38 +0100

    test

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -31,7 +31,7 @@ namespace NameSorter
                 }
 
                 fileService.WriteLines(outputFile, sortedNames);
-                Console.WriteLine($"\n Sorted names written to file {outputFile}");
+                Console.WriteLine($"\n Sorted names written to {outputFile}");
             }
             catch (Exception ex)
             {
